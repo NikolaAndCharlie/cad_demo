36641fab9c1f2777932a2e7ffb5eae923a8af242
zhouxu <zhouxu@simpretech.com>
2020-10-28 17:36:37 +0800

    1.greate a project for parse dxf file.

---
+++ ts/cadplan_zh-cn.ts
@@ -0,0 +1,309 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>ArcEntity</name>
+    <message>
+        <location filename="arc_entity.cc" line="40"/>
+        <source>ARC</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>CircleEntity</name>
+    <message>
+        <location filename="circle_entity.cc" line="23"/>
+        <source>CIR</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EntityView</name>
+    <message>
+        <location filename="entity_view.cc" line="13"/>
+        <source>Hide</source>
+        <translation>隐藏</translation>
+    </message>
+</context>
+<context>
+    <name>LineEntity</name>
+    <message>
+        <location filename="line_entity.cc" line="32"/>
+        <source>LIN</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_window.cc" line="72"/>
+        <source>Items</source>
+        <translation>实体</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="81"/>
+        <source>Use wheel and ctrl+right button to zoom view.</source>
+        <translation>用滚轮缩放，Ctrl加右键适应屏幕。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="291"/>
+        <source>Import</source>
+        <translation>输入</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="295"/>
+        <location filename="main_window.cc" line="298"/>
+        <source>Set</source>
+        <translation>设置</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="312"/>
+        <source>X</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="314"/>
+        <source>Y</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="317"/>
+        <source>Load</source>
+        <translation>载入</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="319"/>
+        <source>DXF</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="321"/>
+        <source>File:</source>
+        <translation>文件：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="324"/>
+        <source>Scale:</source>
+        <translation>缩放：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="326"/>
+        <source>Invert:</source>
+        <translation>翻转：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="333"/>
+        <source>Configure:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="337"/>
+        <location filename="main_window.cc" line="533"/>
+        <location filename="main_window.cc" line="537"/>
+        <source>Region</source>
+        <translation>区域</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="339"/>
+        <source>Width:</source>
+        <translation>宽度：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="341"/>
+        <source>Length%:</source>
+        <translation>长度%：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="351"/>
+        <source>DXF file</source>
+        <translation>DXF 文件</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="361"/>
+        <source>Configure.db file</source>
+        <translation>Configure.db 文件</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="399"/>
+        <source>Display</source>
+        <translation>显示</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="403"/>
+        <source>Line</source>
+        <translation>线</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="417"/>
+        <source>Circle</source>
+        <translation>圆</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="432"/>
+        <source>Arc</source>
+        <translation>弧</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="447"/>
+        <source>All</source>
+        <translation>所有</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="459"/>
+        <source>Clear</source>
+        <translation>清除</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="473"/>
+        <source>Shapes:</source>
+        <translation>形状：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="477"/>
+        <source>Layers:</source>
+        <translation>层：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="488"/>
+        <location filename="main_window.cc" line="494"/>
+        <location filename="main_window.cc" line="504"/>
+        <source>Export</source>
+        <translation>输出</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="495"/>
+        <source>.shm</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="505"/>
+        <source>.drf</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="513"/>
+        <source>.shm file</source>
+        <translation>.shm 文件</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="521"/>
+        <source>.drf file</source>
+        <translation>.drf 文件</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="539"/>
+        <source>Width</source>
+        <translation>宽度</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="543"/>
+        <source>Length Ratio</source>
+        <translation>长度比列</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="549"/>
+        <source>Edge</source>
+        <translation>边缘检测</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="552"/>
+        <source>Auto Strength</source>
+        <translation>自动强度</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="567"/>
+        <source>Lower thresh:</source>
+        <translation>低阈值：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="569"/>
+        <source>Upper thresh:</source>
+        <translation>高阈值：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="571"/>
+        <source>Metric:</source>
+        <translation>过渡尺度：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="573"/>
+        <source>Invalid points:</source>
+        <translation>无效点：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="575"/>
+        <source>Noise:</source>
+        <translation>噪声：</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="643"/>
+        <location filename="main_window.cc" line="674"/>
+        <location filename="main_window.cc" line="679"/>
+        <source>Warning</source>
+        <translation>警告</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="643"/>
+        <location filename="main_window.cc" line="674"/>
+        <source>Set file name first.</source>
+        <translation>首先设置文件名。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="665"/>
+        <location filename="main_window.cc" line="698"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="665"/>
+        <source>Can not create shm file.</source>
+        <translation>无法建立shm文件。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="669"/>
+        <source>SHM Exported.</source>
+        <translation>完成SHM文件输出。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="679"/>
+        <source>Export SHM first.</source>
+        <translation>先输出SHM文件。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="698"/>
+        <source>Can not create DRF file in disk.</source>
+        <translation>无法创建DRF文件。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="704"/>
+        <source>DRF Exported.</source>
+        <translation>DRF文件已输出。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="710"/>
+        <source>Error: set configure.db first.</source>
+        <translation>错误：先设置configure.db文件。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="718"/>
+        <source>Can not open configure.db.</source>
+        <translation>无法打开configure.db文件。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="724"/>
+        <source>Can not read data in configure.db.</source>
+        <translation>无法读取configure.db中的数据。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="734"/>
+        <location filename="main_window.cc" line="747"/>
+        <source>Error in reading DXF.</source>
+        <translation>无法读取DXF文件内容。</translation>
+    </message>
+    <message>
+        <location filename="main_window.cc" line="750"/>
+        <source>Succeed.</source>
+        <translation>成功。</translation>
+    </message>
+</context>
+</TS>
